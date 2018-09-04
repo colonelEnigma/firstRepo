6903a357eba9832d45b650b8ecab7aab0326f7a8
Praveen Ranjan <41191320+colonelEnigma@users.noreply.github.com>
2018-09-05 01:09:12 +0530

    Uploads

---
+++ Dom Game/app.js
@@ -0,0 +1,88 @@
+var scores, roundScore, activePlayer, dice;
+var gamePlaying = true;
+initFun();
+document.querySelector('.btn-roll').addEventListener('click', function(){
+	
+	if(gamePlaying){
+
+		//random number
+	dice = Math.floor(Math.random()*6) + 1;
+	
+	//Display result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-'+ dice + '.png';
+
+	//update the round score
+	if(dice > 1){
+		//add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+	}else{
+		//change active Player
+		nextPlayer();
+	}
+}	
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	if(gamePlaying){
+		//Add current score to global score
+		scores[activePlayer] += roundScore;
+
+		//update information
+		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+		//check if the person won the game
+		if(scores[activePlayer] >= 20){
+			document.getElementById('name-'+ activePlayer).textContent = 'Winner!';
+			document.querySelector('.player-'+ activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-'+ activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+
+
+		}else{
+				//next player
+				nextPlayer();
+		}
+	}
+
+});
+
+function nextPlayer(){
+
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('active');
+		
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', initFun);
+
+function initFun(){
+		gamePlaying = true;
+		scores = [0,0];
+		roundScore= 0;
+		activePlayer = 0;			// 0: active
+
+		document.querySelector('.dice').style.display = 'none';
+		document.getElementById('score-0').textContent = '0';
+		document.getElementById('score-1').textContent = '0';
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		document.getElementById('name-0').textContent = 'Player 1';
+		document.getElementById('name-1').textContent = 'Player 2';
+
+		document.querySelector('.player-0-panel').classList.remove('winner');
+		document.querySelector('.player-1-panel').classList.remove('winner');
+		document.querySelector('.player-0-panel').classList.remove('active');
+		document.querySelector('.player-1-panel').classList.remove('active');
+		document.querySelector('.player-0-panel').classList.add('winner');
+
+}
